bb98d9fec1da477624aebf7fb1f01ed7f4cbbd9f
Daniel Ogbuti <danielogbuti@gmail.com>
2021-10-13 00:47:57 +0100

    reverse_number

---
+++ games/reverse_number.py
@@ -0,0 +1,9 @@
+# reverse_number.py
+
+user_input=int(input("Enter the number to reverse: "))
+_rev=0
+while(user_input>0):
+  dig=user_input%10
+  _rev=_rev*10+dig
+  user_input=user_input//10
+print("The reversed number is :",_rev)
