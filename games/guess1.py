d31b8c8ed5939b4eb208a5019c16b2f692dfab04
vicky6505 <victoriaajao7@gmail.com>
2021-10-12 13:28:46 +0100

    my changes on guess1.py

--- games/guess1.py
+++ games/guess1.py
@@ -5,7 +5,7 @@ def main():
     roll = random.randint(1, 6)
     print(roll)
 #taking user input
-    user_guess = input("Guess a number between 1 and 6: ")
+    user_guess = int(input(("Guess a number between 1 and 6: ")))
     if user_guess == roll:
         print("You guessed correctly!")
     else:
