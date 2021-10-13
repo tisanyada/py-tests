d9fe64771726b4ad45fa53c8c04628889ec68377
Ameya <90517528+AmeyaGawde326@users.noreply.github.com>
2021-10-13 17:06:08 +0530

    Update encryption.py

--- encryption.py
+++ encryption.py
@@ -1,5 +1,3 @@
-import os
-
 key = {'a':'n', 'b':'o', 'c':'p', 'd':'q', 'e':'r', 'f':'s', 'g':'t', 'h':'u', 'i':'v', 'j':'w', 'k':'x', 'l':'y', 'm':'z', 'n':'a',
 'o':'b','p':'c', 'q':'d', 'r':'e', 's':'f', 't':'g', 'u':'h', 'v':'i', 'w':'j', 'x':'k', 'y':'l', 'z':'m', 'A':'N', 'B':'O', 'C':'P',
 'D':'Q', 'E':'R','F':'S', 'G':'T', 'H':'U', 'I':'V', 'J':'W', 'K':'X', 'L':'Y', 'M':'Z', 'N':'A', 'O':'B', 'P':'C', 'Q':'D', 'R':'E',
@@ -26,7 +24,3 @@ for i in pssd:
  ptr.write(i)
 ptr.close()
 print('\nFile Created in /home Directory')
-
-
-
-
